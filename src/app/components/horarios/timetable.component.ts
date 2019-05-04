629b80f60bda7fbc97e958ae2e2dfefd2ea61577
Romano Martin <martinrh83@gmail.com>
2019-05-04 02:28:38 -0300

    Changed creation method of timetable

--- src/app/components/horarios/timetable.component.ts
+++ src/app/components/horarios/timetable.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Component } from '@angular/core';
 import 'rxjs/Rx'
 import { AmazingTimePickerService } from 'amazing-time-picker';
 import { HorariosService } from '../../services/horarios.service';
 import { Timetable } from '../../interfaces/timetable';
 import {ActivatedRoute, Router} from "@angular/router";
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-timetable',
   templateUrl: './timetable.component.html'
 })
 
-export class TimetableComponent implements OnInit {
+export class TimetableComponent {
 
   submitted: boolean = false;
   timetable = new Timetable;
@@ -20,6 +20,7 @@ export class TimetableComponent implements OnInit {
   formError:boolean = false;
   errorData;
   horaSaved:boolean = false;
+  hoursAllowed: Array<any> = ['08:00','08:45','09:30','10:15','11:00','11:45','12:30','13:15','14:00','14:45','15:30','16:15','17:00','17:45','18:30','18:15','19:00','19:45','20:30','21:15','22:00','22:45','23:30'];
   constructor(private atp: AmazingTimePickerService, private _horaService: HorariosService, private router:ActivatedRoute, private _router:Router) {
 
   }
@@ -33,12 +34,12 @@ export class TimetableComponent implements OnInit {
               color: 'white'
           }
       });
-      amazingTimePicker.afterClose().subscribe(time => {
-          this.timetable.hour_init = time;
+      amazingTimePicker.afterClose().subscribe(time => {   
+        this.checkAllowedHours('hour_init', time);
       });
   }
 
-  public getHourEnd() {
+  /*public getHourEnd() {
       const amazingTimePicker = this.atp.open({
           time:  this.hour_end,
           locale: 'es',
@@ -48,16 +49,32 @@ export class TimetableComponent implements OnInit {
           }
       });
       amazingTimePicker.afterClose().subscribe(time => {
-          this.timetable.hour_end = time;
+          //this.timetable.hour_end = time;
+          this.checkAllowedHours('hour_end',time);
       });
+  }*/
+
+  checkAllowedHours(type, time){
+    if(this.hoursAllowed.includes(time)){
+      this.formError = false;
+      this.timetable[type] = time;
+      this.calcHourEnd(time);
+    }else{
+      this.formError = true;
+      this.errorData = [`La hora ${time} no está permitida`];
+    }
   }
 
-  ngOnInit() {
-
+  calcHourEnd(time){
+    var myDate = `2017-04-14 ${time}`; 
+    var hour_end = moment(myDate).add(45, 'm').format('HH:mm');
+    //console.log(hour_end);
+    this.timetable.hour_end = hour_end;
   }
 
   createTimetable(timetable){
     this.submitted = true;
+    //console.log(timetable);
     this._horaService.createTimetable(timetable).subscribe(data => {
       if(data.error){
         this.formError = true;
@@ -66,7 +83,7 @@ export class TimetableComponent implements OnInit {
         this.horaSaved = true;
         setTimeout(() => {
           this._router.navigate(['/horas']);
-        }, 2500);
+        }, 1500);
       }
     });
   }
